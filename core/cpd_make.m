9602deda5f8116e4ebc0cadc20ce303c27557370
Christoph Siedentop <christoph.siedentop@iwr.uni-heidelberg.de>
2014-06-27 15:39:36 +0200

    cpd_make: fixed Matlab-API change on fileparts().

--- core/cpd_make.m
+++ core/cpd_make.m
@@ -20,7 +20,9 @@
 
 function cpd_make()
 
-psave=pwd; p = mfilename('fullpath'); [pathstr, name, ext, versn] = fileparts(p);
+psave=pwd;
+p = mfilename('fullpath');
+[pathstr, name, ext] = fileparts(p);
 
 %%%%%%%%%%%%%%%%%%%% cpd_P %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
